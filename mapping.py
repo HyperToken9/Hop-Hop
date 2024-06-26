5f973a24624c7c68a0c53998e802293292d9ef9d
Nathan Adrian <nathanadrian2002@gmail.com>
2024-06-26 11:32:37 +0530

    Ooh How i have missed snakecase

---
+++ mapping.py
@@ -0,0 +1,17 @@
+
+""" 
+Module to mange and handle all the mapping operations
+- Distance
+- ETA ... ETC
+"""
+
+
+location_a = [19.180805197363586, 72.83691196364899]
+location_b = [18.94436356365018, 72.8287156515625]
+
+
+def eta_from_gps_coordinates(location_a, location_b):
+    """
+    Calculate the ETA between two GPS coordinates
+    """
+    pass
